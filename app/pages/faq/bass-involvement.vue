c3f04d6dcfe16a7df45f088fc8fd84ca19f1b24b
Clarissa Merz <clarissamerz@gmail.com>
2026-01-26 16:25:17 +0100

    add new subpage

---
+++ app/pages/faq/bass-involvement.vue
@@ -0,0 +1,24 @@
+<script setup>
+definePageMeta({
+    layout: 'faq',
+});
+</script>
+
+<template>
+    <div>
+        <Heading>{{ $t('bassInvolvement') }}</Heading>
+
+        <p>
+            Um blablabla etwas mit Bassbeteiligung und Synkopenketten.
+            
+            Für diese Auswertung wurde blabla.
+            
+            Auf diese Weise wurden von insgesamt xxxxxx
+            Anfängen xxxxxx Stücke gefunden, bei
+            denen unmittelbar zu Beginn ein Zusammenklang auftritt.
+        </p>
+        <p>
+            Hier kommt irgendein anderer Text.
+        </p>
+    </div>
+</template>
